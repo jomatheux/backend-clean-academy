8ccfeb869d9b18b1efc225a51bbfa20279c16aa5
jomatheux <jomatheux@gmail.com>
2025-04-04 22:14:17 -0300

    adjust commit

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,7 +13,7 @@ const corsOptions = {
     origin: ['http://localhost:3000', 'https://new-clean-academy.vercel.app'], // Permite apenas esta origem
     credentials: true, // Permite cookies e cabeçalhos de autorização
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
 };
 
 app.use(cors(corsOptions));
