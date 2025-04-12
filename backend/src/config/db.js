ff440396a18364611735f48de50bcb29e154e030
jomatheux <jomatheux@gmail.com>
2025-04-12 14:05:04 -0300

    alterações no docker compose e db

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -5,13 +5,23 @@ const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.D
   host: process.env.DB_HOST,
   port: process.env.DB_PORT || 3306,
   dialect: process.env.DB_DIALECT,
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+    retry: {
+      max: 5 // Tentar reconectar até 5 vezes
+    }
+  },
 });
 
 try {
-    db.authenticate();
+    await db.authenticate();
     console.log('Conexão com o banco de dados realizada com sucesso!');
 } catch (error) {
     console.log(`Não foi possível conectar: ${error}`);
+    setTimeout(connectDatabase, 5000); // Tentar novamente após 5 segundos
 }
 
 export default db;
